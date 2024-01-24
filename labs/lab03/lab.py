e5f86eaa029cad5685004fefcc7f2c7999b5c749
Mac.Ari <d.aritra@outlook.com>
2024-01-23 19:55:03 -0800

    Lab 3

---
+++ labs/lab03/lab.py
@@ -0,0 +1,69 @@
+# lab.py
+
+
+import os
+import io
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+
+# ---------------------------------------------------------------------
+# QUESTION 1
+# ---------------------------------------------------------------------
+
+
+def read_linkedin_survey(dirname):
+    ...
+
+
+def com_stats(df):
+    ...
+
+
+
+# ---------------------------------------------------------------------
+# QUESTION 2
+# ---------------------------------------------------------------------
+
+
+def read_student_surveys(dirname):
+    ...
+
+
+def check_credit(df):
+    ...
+
+
+# ---------------------------------------------------------------------
+# QUESTION 3
+# ---------------------------------------------------------------------
+
+
+def most_popular_procedure(pets, procedure_history):
+    ...
+
+def pet_name_by_owner(owners, pets):
+    ...
+
+
+def total_cost_per_city(owners, pets, procedure_history, procedure_detail):
+    ...
+
+
+# ---------------------------------------------------------------------
+# QUESTION 4
+# ---------------------------------------------------------------------
+
+
+def average_seller(sales):
+    ...
+
+def product_name(sales):
+    ...
+
+def count_product(sales):
+    ...
+
+def total_by_month(sales):
+    ...
