f333ef6843ad191378e00362d594bfcb959da343
Mac.Ari <d.aritra@outlook.com>
2024-02-14 10:08:31 -0800

    Lab 6 Release

---
+++ labs/lab06/lab.py
@@ -0,0 +1,64 @@
+# lab.py
+
+
+import os
+import pandas as pd
+import numpy as np
+import requests
+import bs4
+import lxml
+
+
+# ---------------------------------------------------------------------
+# QUESTION 1
+# ---------------------------------------------------------------------
+
+
+def question1():
+    """
+    NOTE: You do NOT need to do anything with this function.
+    The function for this question makes sure you
+    have a correctly named HTML file in the right
+    place. Note: This does NOT check if the supplementary files
+    needed for your page are there!
+    """
+    # Don't change this function body!
+    # No Python required; create the HTML file.
+    return
+
+
+# ---------------------------------------------------------------------
+# QUESTION 2
+# ---------------------------------------------------------------------
+
+
+
+def extract_book_links(text):
+    ...
+
+def get_product_info(text, categories):
+    ...
+
+def scrape_books(k, categories):
+    ...
+
+
+# ---------------------------------------------------------------------
+# QUESTION 3
+# ---------------------------------------------------------------------
+
+
+def stock_history(ticker, year, month):
+    ...
+
+def stock_stats(history):
+    ...
+
+
+# ---------------------------------------------------------------------
+# QUESTION 4
+# ---------------------------------------------------------------------
+
+
+def get_comments(storyid):
+    ...
